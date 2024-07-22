1a01c9bcd33e98d2d0cb67b690f2b3cb9b7fb48e
Tushar Patel <86516517+Tushar0589@users.noreply.github.com>
2024-07-22 09:18:50 +0530

    Add files via upload

---
+++ color_circle.py
@@ -0,0 +1,29 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor('black')
+
+turtle.speed(0.5)
+for i in range(1,25):
+    turtle.color('red')
+    turtle.circle(100)
+    turtle.right(21)
+    turtle.color('blue')
+    turtle.circle(100)
+    turtle.right(21)
+    turtle.color('green')
+    turtle.circle(100)
+    turtle.right(21)
+    turtle.color('orange')
+    turtle.circle(100)
+    turtle.right(21)
+    turtle.color('yellow')
+    turtle.circle(100)
+    turtle.right(21)
+    turtle.color('violet')
+    turtle.circle(100)
+    turtle.color('purple')
+    turtle.circle(100)
+    
+
+turtle.done()
